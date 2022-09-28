6334698842df7002ce7d8cd443acd1718e0ec0d7
chirag bhalotia <me@chirag.codes>
2022-09-28 21:00:36 +0530

    loops

---
+++ loops.py
@@ -0,0 +1,14 @@
+for ele in range(1,6):
+    print(ele,end=", ")
+
+i = 1
+print()
+while( i <= 5 ):
+    print(i, end = " ")
+    i=i+1
+
+print()
+for ele in range(2,10,3):
+    print(ele, end = " : ")
+
+
