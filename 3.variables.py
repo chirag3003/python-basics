b60323bc03f94ca1cb59aab2f2b4f435c19c0a97
chirag bhalotia <me@chirag.codes>
2022-09-28 20:49:10 +0530

    variables

---
+++ 3.variables.py
@@ -0,0 +1,23 @@
+#Assigning value to i and printing its value
+i = 5
+print(5)
+
+#printing the type of variable i
+print(type(i))
+
+#setting value of i to a different data type
+i = "Hello World"
+print(i)
+print(type(i))
+
+i = 45.5
+print(i)
+print(type(i))
+
+i = False
+print(i)
+print(type(i))
+
+i = None 
+print(i)
+print(type(i))
