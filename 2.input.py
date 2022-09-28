384972ec4748ccb79a0012aacd303a63cab0de9a
chirag bhalotia <me@chirag.codes>
2022-09-28 20:48:51 +0530

    Input in python

---
+++ 2.input.py
@@ -0,0 +1,8 @@
+#taking input from the user and printing its type
+name = input("Enter Your Name");
+print("You entered the following name:",name)
+print(type(name)) #input always returns strings
+
+
+
+
