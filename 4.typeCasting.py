5449da7ed4a81f2fc1f0d571dd839812916c0688
chirag bhalotia <me@chirag.codes>
2022-09-28 20:49:34 +0530

    typeCasting

---
+++ 4.typeCasting.py
@@ -0,0 +1,13 @@
+num = '8'
+print(num)
+print(type(num))
+
+#Converting from string to integer
+num = int(num)
+print(num)
+print(type(num))
+
+#Converting from integer to float
+num = float(num)
+print(num)
+print(type(num))
